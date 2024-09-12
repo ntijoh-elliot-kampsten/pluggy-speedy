ad391c8da31ecf53b70312eda4e3aa67144cacfe
Linus0812 <linus.andersson.0812@outlook.com>
2024-09-12 13:24:52 +0200

    checkout done typ

--- lib/pluggy/models/checkout.ex
+++ lib/pluggy/models/checkout.ex
@@ -1,10 +1,13 @@
 defmodule Pluggy.Checkout do
+  alias Pluggy.Order
+
   # Function to get the current order for a user
   def get_current_order(user_id) do
     query = """
     SELECT * FROM orders WHERE user_id = $1
     """
     Postgrex.query!(DB, query, [user_id]).rows
+    |> Order.parse_data
   end
 
   # Function to finalize an order
@@ -13,5 +16,13 @@ defmodule Pluggy.Checkout do
     UPDATE orders SET state = 'Registered' WHERE id = $1
     """
     Postgrex.query!(DB, query, [order_id])
+  end
+
+  # Function to remove a pizza from an order
+  def remove_pizza(order_id, pizza_id) do
+    query = """
+    DELETE FROM order_items WHERE order_id = $1 AND pizza_id = $2
+    """
+    Postgrex.query!(DB, query, [order_id, pizza_id])
   end
 end
