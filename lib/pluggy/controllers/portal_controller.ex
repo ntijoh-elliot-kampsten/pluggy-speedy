e9d76b6736b36a79d206120b79168df26b8c8f6a
wonkhester <kevinradley@outlook.com>
2024-09-16 10:01:00 +0200

    added portal controller

---
+++ lib/pluggy/controllers/portal_controller.ex
@@ -0,0 +1,28 @@
+defmodule Pluggy.PortalController do
+  require IEx
+
+  alias Pluggy.User
+  import Plug.Conn, only: [send_resp: 3]
+
+  def index(conn, function) do
+    # Get user if logged in
+    session_user = conn.private.plug_session["user_id"]
+
+    case session_user do
+      nil ->
+        redirect(conn, "/login")
+      _ ->
+        case User.exist(session_user) do
+          false ->
+            redirect(conn, "/login")
+          true ->
+            # User is logged in, execute the passed function
+            function.(conn)
+        end
+    end
+  end
+
+  defp redirect(conn, url) do
+    Plug.Conn.put_resp_header(conn, "location", url) |> send_resp(303, "")
+  end
+end
--- lib/pluggy/router.ex
+++ lib/pluggy/router.ex
@@ -7,6 +7,7 @@ defmodule Pluggy.Router do
   alias Pluggy.PizzaController
   alias Pluggy.UserController
   alias Pluggy.CheckoutController
+  alias Pluggy.PortalController
 
   plug(Plug.Static, at: "/", from: :pluggy)
   plug(:put_secret_key_base)
@@ -34,21 +35,21 @@ defmodule Pluggy.Router do
   plug(:match)
   plug(:dispatch)
 
-  get("/", do: PizzaController.index(conn))
-  get("/main", do: PizzaController.index(conn))
+  get("/", do: PortalController.index(conn, fn conn -> PizzaController.index(conn) end))
+  get("/main", do: PortalController.index(conn, fn conn -> PizzaController.index(conn) end))
 
-  get("/orders", do: OrderController.show_orders(conn))
-  get("/order/update", do: Order.update_order("Carl Svensson"))
-  post("/order/add", do: OrderController.create(conn, conn.body_params))
-  post("/order/change-state", do: OrderController.change_state(conn, conn.body_params))
-  post("/order/remove-order", do: OrderController.remove_order(conn, conn.body_params))
-  post("/order/remove-order-part", do: OrderController.remove_order_part(conn, conn.body_params))
+  get("/orders", do: PortalController.index(conn, fn conn -> OrderController.show_orders(conn) end))
+  get("/order/update", do: PortalController.index(conn, fn conn -> Order.update_order("Carl Svensson") end))
+  post("/order/add", do: PortalController.index(conn, fn conn -> OrderController.create(conn, conn.body_params) end))
+  post("/order/change-state", do: PortalController.index(conn, fn conn -> OrderController.change_state(conn, conn.body_params) end))
+  post("/order/remove-order", do: PortalController.index(conn, fn conn -> OrderController.remove_order(conn, conn.body_params) end))
+  post("/order/remove-order-part", do: PortalController.index(conn, fn conn -> OrderController.remove_order_part(conn, conn.body_params) end))
 
-  get("/customize/:id", do: PizzaController.customize(conn, id))
+  get("/customize/:id", do: PortalController.index(conn, fn conn -> PizzaController.customize(conn, id) end))
 
-  get("/checkout", do: CheckoutController.index(conn))
-  post("/checkout/finalize", do: CheckoutController.finalize(conn, conn.body_params))
-  get("/order_confirmation", do: CheckoutController.confirmation(conn))
+  get("/checkout", do: PortalController.index(conn, fn conn -> CheckoutController.index(conn) end))
+  post("/checkout/finalize", do: PortalController.index(conn, fn conn -> CheckoutController.finalize(conn, conn.body_params) end))
+  get("/order_confirmation", do: PortalController.index(conn, fn conn -> CheckoutController.confirmation(conn) end))
     # get("/fruits", do: FruitController.index(conn))
   # get("/fruits/new", do: FruitController.new(conn))
   # get("/fruits/:id", do: FruitController.show(conn, id))
@@ -65,7 +66,7 @@ defmodule Pluggy.Router do
 # User management routes
 get("/login", do: UserController.login_form(conn))
 get("/signup", do: UserController.signup_form(conn))
-get("/change_password", do: UserController.change_password_form(conn))
+get("/change_password", do: PortalController.index(conn, fn conn -> UserController.change_password_form(conn) end))
 post("/users/login", do: UserController.login(conn, conn.body_params))
 post("/users/logout", do: UserController.logout(conn)) # This is correct
 post("/users/signup", do: UserController.signup(conn, conn.body_params))
